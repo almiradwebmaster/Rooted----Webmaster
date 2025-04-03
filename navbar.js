2914983796bc6fd35c599dcb5503a8876ac80e36
almiradwebmaster <danayeva.almira@gmail.com>
2025-04-02 22:45:52 -0400

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.body.insertAdjacentHTML("afterbegin", `
+        <header>
+            <nav class = "navbar">
+                <div class = "logo"><img src="logo.png" alt="ROOTED" height = "75px" width = "143px"></div>
+                <ul class = "nav-links">
+                    <li><a href = "index.html">Menu</a></li>
+                    <li><a href = "aboutus.html">About Us</a></li>
+                 </ul>
+            </nav>
+        </header>
+        `);
+});
